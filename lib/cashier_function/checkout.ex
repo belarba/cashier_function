6ec19d72dcd8a9633d4f42196691fde4008b7b9e
Daniel Belarmino <belarba@gmail.com>
2024-07-05 13:34:37 +0100

    typo fix and initial checkout function

---
+++ lib/cashier_function/checkout.ex
@@ -0,0 +1,24 @@
+defmodule CashierFunction.Checkout do
+  @moduledoc """
+  Module to calculate the total price of a basket of items
+  """
+
+  @prices %{"GR1" => 3.11, "SR1" => 5.00, "CF1" => 11.23}
+
+  def total_price(items) do
+    items
+    |> Enum.frequencies()
+    |> Enum.reduce(0, fn {code, quantity}, acc ->
+      acc + calculate_price(code, quantity)
+    end)
+    |> Float.round(2)
+  end
+
+  defp calculate_price("SR1", quantity) when quantity >= 3 do
+    4.50 * quantity
+  end
+
+  defp calculate_price(code, quantity) do
+    @prices[code] * quantity
+  end
+end
--- test/cashier_function_test.exs
+++ test/cashier_function_test.exs
@@ -3,18 +3,18 @@ defmodule CashierFunctionTest do
   doctest CashierFunction
 
   test "Basket: GR1, SR1, GR1, GR1, CF1 - Total price expected: £22.45" do
-    assert CahsierFunction.Checkout.total_price(["GR1", "SR1", "GR1", "GR1", "CF1"]) == 22.45
+    assert CashierFunction.Checkout.total_price(["GR1", "SR1", "GR1", "GR1", "CF1"]) == 22.45
   end
 
   test "Basket: GR1, GR1 - Total price expected: £3.11" do
-    assert CahsierFunction.Checkout.total_price(["GR1", "GR1"]) == 3.11
+    assert CashierFunction.Checkout.total_price(["GR1", "GR1"]) == 3.11
   end
 
   test "Basket: SR1, SR1, GR1, SR1 - Total price expected: £16.61" do
-    assert CahsierFunction.Checkout.total_price(["SR1", "SR1", "GR1", "SR1"]) == 16.61
+    assert CashierFunction.Checkout.total_price(["SR1", "SR1", "GR1", "SR1"]) == 16.61
   end
 
   test "Basket: GR1, CF1, SR1, CF1, CF1 - Total price expected: £30.57" do
-    assert CahsierFunction.Checkout.total_price(["GR1", "CF1", "SR1", "CF1", "CF1"]) == 30.57
+    assert CashierFunction.Checkout.total_price(["GR1", "CF1", "SR1", "CF1", "CF1"]) == 30.57
   end
 end
